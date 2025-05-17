54b7bc2c8e8cdc0b11109908e6e4baf266b7f167
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-05-17 14:29:12 +0300

    Create Time_Calc_Proj.py
    Trying to write this code and learning how to add the two times to each other

---
+++ Time_Calc_Proj.py
@@ -0,0 +1,10 @@
+def add_time(start, duration, day=""):
+    days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saterday", "Sunday"]
+    
+    time = start.strip().split(" ")
+    print(time)
+    
+    
+    # return new_time
+
+# add_time("3:00 PM", 2)
