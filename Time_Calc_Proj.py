b285d7af158c17a1e294863846369508f510b149
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-05-18 14:33:24 +0300

    Time_Calc_proj.py Update 1.1
    Adding exception handling to handle the cases when is there is no period entered.
    Stating to make the logic for adding the time and the duration together.

--- Time_Calc_Proj.py
+++ Time_Calc_Proj.py
@@ -1,19 +1,21 @@
 def add_time(start, duration, day=""):
     # days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saterday", "Sunday"]
-    
-    time_part_1, period_1 = start.strip().split()
-    start_hour, start_minute = map(int, time_part_1.strip().split(":")) #this line converts the list of ["3","45"] for integers
-    while True:
+    try:
+        time_part_1, period_1 = start.strip().split()
+        start_hour, start_minute = map(int, time_part_1.strip().split(":")) #this line converts the list of ["3","45"] for integers
         if start_minute >= 60:
             print("print the minutes must be less than 60!")
-            continue
-        
-        duration_hour, duration_minute = map(int, duration.strip().split(":"))
-        if duration_minute >= 60:
-            print("print the minutes must be less than 60!")
-            continue
-        
-        new_hour = start_hour + duration_hour
-        new_minute = start_minute + duration_minute
+            return
+    except Exception:
+        print("The must contain the period.")
+        return
     
-add_time(input("Please enter the starter time 'hh:mm PM/AM': "), input("Please enter the duration to be added 'hh:mm': "))
+    duration_hour, duration_minute = map(int, duration.strip().split(":"))
+    if duration_minute >= 60:
+        print("print the minutes must be less than 60!")
+        return
+
+    new_hour = start_hour + duration_hour
+    new_minute = start_minute + duration_minute
+    
+add_time("3:00 ", "2:30")
