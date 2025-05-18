5ab32da79dd78282c595f9c88949fae8e77a0a95
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-05-18 12:26:17 +0300

    Time_Calc_proj.py Update 1.0
    Handling the format of the time input and trying not to take minutes bigger than 60.

--- Time_Calc_Proj.py
+++ Time_Calc_Proj.py
@@ -1,10 +1,19 @@
 def add_time(start, duration, day=""):
-    days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saterday", "Sunday"]
+    # days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saterday", "Sunday"]
     
-    time = start.strip().split(" ")
-    print(time)
+    time_part_1, period_1 = start.strip().split()
+    start_hour, start_minute = map(int, time_part_1.strip().split(":")) #this line converts the list of ["3","45"] for integers
+    while True:
+        if start_minute >= 60:
+            print("print the minutes must be less than 60!")
+            continue
+        
+        duration_hour, duration_minute = map(int, duration.strip().split(":"))
+        if duration_minute >= 60:
+            print("print the minutes must be less than 60!")
+            continue
+        
+        new_hour = start_hour + duration_hour
+        new_minute = start_minute + duration_minute
     
-    
-    # return new_time
-
-# add_time("3:00 PM", 2)
+add_time(input("Please enter the starter time 'hh:mm PM/AM': "), input("Please enter the duration to be added 'hh:mm': "))
